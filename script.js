f0b5926b03844db3ede71f7a8e4d55a6cac905ac
RaissaLeaneNogueira <126248747+RaissaLeaneNogueira@users.noreply.github.com>
2024-06-18 10:57:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const caixaPrincipal =document.querySelector(".caixa-principal");
 const caixaPerguntas =document.querySelector(".caixa-perguntas");
 const caixaAlternativas =document.querySelector(".caixa-alternativas");
+const caixaResultado =document.querySelector(".caixa-resultado");
 
